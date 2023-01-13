8d9ff59e57c1fe01bce9a6d8e652de496f172666
HarveyBurton96 <harvey.burton@sky.com>
2023-01-13 18:00:35 +0000

    Add users comments and like/unlike option

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,6 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['django-blog16.herokuapp.com', 'localhost']
 
+# EMAIL - added by HB
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Application definition
 
@@ -40,12 +43,24 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
